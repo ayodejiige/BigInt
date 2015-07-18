f7c70abaf848ca1f4d7542da37a13095853bf85f
ayodejiige <ayodeji18@outlook.com>
2015-07-18 07:26:44 -0400

    Updated header file and added .cpp file

---
+++ BigInt/big_int.cpp
@@ -0,0 +1,27 @@
+#include "big_int.h"
+
+typedef BigInt::DataType DataType;
+
+BigInt::BigInt() {
+	
+}
+
+BigInt::BigInt(int val) {
+
+}
+
+BigInt::size() const {
+	return size_;
+}
+
+BigInt BigInt::operator +(BigInt num) {
+
+}
+
+std::istream& operator>> (std::istream& in, BigInt& num) {
+
+}
+
+std::ostream& operator<< (std::ostream& out, const BigInt& num) {
+	
+}
