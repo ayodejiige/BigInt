56cb602ab036c67492a9663515e1a57c04895419
ayodejiige <ayodeji18@outlook.com>
2015-07-19 06:45:57 -0400

    Updated .cpp file

--- BigInt/big_int.cpp
+++ BigInt/big_int.cpp
@@ -1,13 +1,21 @@
 #include "big_int.h"
 
+#include <vector>
+
 typedef BigInt::DataType DataType;
 
 BigInt::BigInt() {
-	
+	big_int_.push_back(0);
+	size_ = 1;
+	signed_ = false;
 }
 
 BigInt::BigInt(int val) {
-
+	int temp = val;
+	while (temp!=0) {
+		big_int_.push_back(temp%10);
+		temp = temp/10;
+	}
 }
 
 BigInt::size() const {
@@ -15,7 +23,7 @@ BigInt::size() const {
 }
 
 BigInt BigInt::operator +(BigInt num) {
-
+	
 }
 
 std::istream& operator>> (std::istream& in, BigInt& num) {
