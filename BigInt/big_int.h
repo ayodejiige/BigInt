9ada3fa6350bf0841816d77a4e9937044dbe2d37
ayodejiige <ayodeji18@outlook.com>
2015-07-23 22:14:42 -0400

    Implemented more functions, negative numbers capability

--- BigInt/big_int.h
+++ BigInt/big_int.h
@@ -2,47 +2,67 @@
 #define BIGINT_H
 
 // C++ Libraries
+#include <cstdlib>
 #include <vector>
 #include <iostream>
 
-//Insert class definition
- 
+// Insert class definition
+
 class BigInt {
+ public:
+  // Default constructor creates big integer 0
+  BigInt();
+  // Constructor creates big integer from integer given
+  BigInt(int val);
+  // Destructor
+  ~BigInt();
+  // Copy Constructor
+  BigInt(const BigInt& source);
 
-public:
-	
-	// Default constructor creates big integer 0
-	BigInt();
-	// Constructor creates big integer from integer given
-	BigInt(int val);
-	// Destructor 
-	// Might not need this
-	//~BigInt();
-	
-	// Returns the number of digits in the big integer
-	unsigned int size() const;
+  // Returns the number of digits in the big integer
+  unsigned int size() const;
 
-	// Overload operators for arithmetic operations
+  // Assignment operator
+  BigInt operator=(const BigInt& rvalue);
+  BigInt operator=(const int& rvalue);
 
-	// Adds big integers together
-	BigInt operator +(const BigInt& num);
-	// Returns true if 
-	// bool operator <(const BigInt &num);
-	// Returns false if 
-	// bool operator >(const BigInt &num);
+  // Basic arithmetic operators
+  BigInt operator+(const BigInt& rvalue) const;
+  BigInt operator-(const BigInt& rvalue) const;
+  // BigInt operator *(const BigInt& rvalue);
+  // BigInt operator /(const BigInt& rvalue);
+  // BigInt operator %(const BigInt& rvalue);
 
-	// Overload I/O operators
-	friend std::istream& operator >>(std::istream& in, const BigInt& num);
-	friend std::ostream& operator <<(std::ostream& out, const BigInt& num);
+  BigInt operator+=(const BigInt& rvalue);
+  // void operator -=(const BigInt& rvalue);
+  // void operator *=(const BigInt& rvalue);
+  // void operator /=(const BigInt& rvalue);
 
-private:
-	// Represention of big integer as a vector of numbers
-	std::vector<int> data_;
-	// Current number of digits in big integer
-	unsigned int size_;
-	// True if big integer is negative
-	bool signed_;
-	//BigInt max (const BigInt &num, const BigInt &num);
+  // Boolean operators
+  bool operator==(const BigInt& rvalue) const;
+  bool operator!=(const BigInt& rvalue) const;
+  bool operator<=(const BigInt& rvalue) const;
+  bool operator>=(const BigInt& rvalue) const;
+  bool operator<(const BigInt& rvalue) const;
+  bool operator>(const BigInt& rvalue) const;
+
+  // I/O operators
+  friend std::istream& operator>>(std::istream& in, const BigInt& rvalue);
+  friend std::ostream& operator<<(std::ostream& out, const BigInt& rvalue);
+
+ private:
+  // Member variable
+
+  // Represention of big integer as a vector of numbers
+  std::vector<int> data_;
+  // Current number of digits in big integer
+  unsigned int size_;
+  // True if big integer is negative
+  bool signed_;
+  // BigInt max (const BigInt &rvalue, const BigInt &rvalue);
+
+  // Helper functions
+  BigInt switch_sign(BigInt value) const;
 };
- 
-#endif // BIGINT_H
+
+#endif  // BIGINT_H
--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -5,15 +5,23 @@
 
 int main() {
 
-
-	BigInt a(5500);
-
-	BigInt b(22);
-
-	std::cout << a + b;
-
-    system("pause");
-	return 0;
+  BigInt a(5500);
+  BigInt b(22);
+  BigInt empty;
+  empty = b-a;
+  std::cout << "a is " << a << std::endl;
+  std::cout << "b is " << b << std::endl;
+  std::cout<< "empty is" << empty << std::endl;
+  empty += a;
+  std::cout<< "empty is now" << empty << std::endl;
+  std::cout << "We got b - a" << b - a << std::endl;
+  bool my_bool = a > b;
+  std::cout << "is a > b " << my_bool << std::endl;
+  my_bool = a < b;
+  std::cout << "is a < b " << my_bool << std::endl;
+  my_bool = a < BigInt(-1);
+  std::cout << "is a < -1 " << my_bool << std::endl;
+  my_bool = a==b;
+  std::cout << "is a == b " << my_bool<< std::endl;
+  system("pause");
 }
-
-
